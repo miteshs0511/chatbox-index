16f775a6edb01121a2f44adaa646eeb4ead55a75
miteshs051 <miteshsanchaniya051@gmail.com>
2021-07-11 07:32:59 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ChatBotService } from './chat-bot.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chatbot';
+
+  // showChatBot: boolean = false;
+
+  constructor(public chatBotService: ChatBotService){
+
+
+  }
+
+  ngOnInit(){
+
+  }
+
+  chatClick(){
+    this.chatBotService.showChatBot = true;
+  }
 }
